294034e81564b010c6025cb46081c4f4829ea3d8
lifegrain <terrykho@ymail.com>
2020-04-20 16:32:07 +0700

    Get User Data update
    server\controllers\userController.js
    - added get User data
    - left join Music
    - left join Comment → left join Music → left join User
    - left join Rating → left join Music → left join User
    
    server\models\User.js
    - fixed wrong association
    
    server\routes\userRoutes.js
    - added route for get user data

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,4 +1,4 @@
-const { User, Comment } = require(`../models`);
+const { User, Comment, Music, Rating } = require(`../models`);
 const createError = require(`http-errors`);
 const jwt = require(`../helpers/jwt`);
 const bcrypt = require(`../helpers/bcrypt`);
@@ -44,6 +44,72 @@ class UserController {
 				} else {
 					throw createError(400, `Wrong Username/Password`);
 				}
+			})
+			.catch(next);
+	}
+
+	static getUser(req, res, next) {
+		let id = Number(req.params.id);
+
+		User.findByPk(id, {
+			attributes: {
+				exclude: `password`,
+			},
+			include: [
+				{
+					model: Music,
+					attributes: {
+						exclude: `UserId`,
+					},
+				},
+				{
+					model: Comment,
+					attributes: {
+						exclude: [`UserId`, `MusicId`],
+					},
+					include: [
+						{
+							model: Music,
+							attributes: {
+								exclude: `UserId`
+							},
+							include: [
+								{
+									model: User,
+									attributes: {
+										exclude: `password`
+									}
+								}
+							]
+						}
+					],
+				},
+				{
+					model: Rating,
+					attributes: {
+						exclude: [`UserId`, `MusicId`]
+					},
+					include: [
+						{
+							model: Music,
+							attributes: {
+								exclude: `UserId`
+							},
+							include: [
+								{
+									model: User,
+									attributes: {
+										exclude: `password`
+									}
+								}
+							]
+						}
+					]
+				}
+			],
+		})
+			.then((data) => {
+				res.status(200).json(data);
 			})
 			.catch(next);
 	}
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,6 +1,7 @@
 const router = require(`express`).Router();
 const { User } = require(`../controllers`);
 
+router.get(`/:id`, User.getUser);
 router.post(`/register`, User.register);
 router.post(`/login`, User.login);
 
